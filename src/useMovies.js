90f76274654e02ce1d25e2d20425971797f57b10
Peter Farkas <pjf3av@gmail.com>
2024-07-26 00:09:05 -0400

    fixed api issue

--- src/useMovies.js
+++ src/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
             { signal: controller.signal }
           );
 
